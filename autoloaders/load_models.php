14c31b14f74c6ccc26907b62b1b59bd5e4f22819
Livingstone <43893458+lividavi19@users.noreply.github.com>
2023-09-11 18:11:00 +0300

    Delete autoloaders directory

--- autoloaders/load_models.php
+++
@@ -1,9 +0,0 @@
-<?php
-    spl_autoload_register (function ($class_name) {
-        // check if file exists
-        $file = $_SERVER['DOCUMENT_ROOT']."/models/$class_name.php";
-        if(file_exists($file)){
-            require_once $file;
-        }
-    });
-?>
