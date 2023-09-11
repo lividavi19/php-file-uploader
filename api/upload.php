62731f524471a447c09b28dcae76ecbfbc577b54
Livingstone <43893458+lividavi19@users.noreply.github.com>
2023-09-11 18:10:46 +0300

    Delete api/upload.php

--- api/upload.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require_once $_SERVER['DOCUMENT_ROOT'].'/autoloaders/load_models.php';
-    @$file_input = $_FILES['file_input'];
-
-    $uploader = new FileUploader($file_input);
-    $uploader->upload();
-?>
